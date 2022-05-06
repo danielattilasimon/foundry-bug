9cdec271af4e0c968108760138fade24ac1db1a4
Daniel Simon <daniel.attila.simon@gmail.com>
2022-05-06 12:33:08 +0700

    test: add a library to demonstrate compilation failure

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "./utils/Library.sol";
 
 contract ContractTest is Test {
     function setUp() public {}
 
     function testExample() public {
-        assertTrue(true);
+        uint foo = Library.foo();
+        assertEq(foo, 1337);
     }
 }
---
+++ test/utils/Library.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.13;
+
+library Library {
+    function foo() external pure returns (uint) {
+        return 1337;
+    }
+}
